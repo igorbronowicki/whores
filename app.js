86c5157d7e9bfd07db9fbdada33ba46aca2587bc
Kill <streetridebmx@gmail.com>
2013-08-31 14:36:11 +0300

    add dummy db, routes, inner tpls

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ ejs.close = '%]';
 var express = require('express');
 var app = express();
 
+var db = require('./db');
 
 // app configuration
 app.engine('html', require('ejs').__express);
@@ -12,14 +13,25 @@ app.set('view engine', 'html');
 app.use('/static', express.static(__dirname + '/public'));
 
 
-// Главная страница сайта
+// Routes
 app.get('/', function(req, res) {
     res.render('index', {
         title: "Hello world!"
     });
 });
+app.get('/whores', function(req, res) {
+    res.render('whores', {
+        title: "All Whores",
+        whores: db.getAllWhores()
+    });
+});
+app.get('/whores/:id', function(req, res) {
+    var id = req.params.id;
+    res.render('whore', {
+        whore: db.getWhoreById(id)
+    });
+});
 
-
-// 
+// Config server port
 app.listen(3000);
 console.log('check 127.0.0.1:3000 out');
--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-console.log("Hello world!");
+// var express = require('express');
+// var app = express().createServer();
+
+// // app configuration
+// app.engine('html', require('ejs').__express);
+// app.set('views', __dirname + '/templates');
+// app.set('view engine', 'html');
+// app.use('/static', express.static(__dirname + '/public'));
+
+// //girl routes
+// app.get('/whores/:id', function(req, res){
+//     res.send('whores ' + req.params.id);
+// });
